8f503748b63429558f8351e3937954b445a1caf9
praffulTripathi <praffulla.tripathi@gmail.com>
2023-05-21 13:32:20 +0530

    Set State for SVGs

--- twitterapp/src/App/LeftPanel/ListItems.js
+++ twitterapp/src/App/LeftPanel/ListItems.js
@@ -1,29 +1,61 @@
-import { useContext, useState, useEffect } from 'react';
-import { APIResponseContext } from '../Providers/APIContext';
-import SvgIcon from './SvgIcon';
-
+import { useContext, useState, useEffect } from "react";
+import { APIResponseContext } from "../Providers/APIContext";
+import SvgIcon from "./SvgIcon";
 
 function ListItems() {
+  const [listItems, setListItems] = useState(null);
+  const [listSVGs, setListSVGs] = useState(null);
+  const apiResponse = useContext(APIResponseContext);
 
-    const [listItems, setListItems] = useState(null);
-    const apiResponse = useContext(APIResponseContext);
+  useEffect(() => {
+    setListItems(apiResponse?.["sideNavigationButtons"]);
 
-    useEffect(() => {
-        setListItems(apiResponse?.["sideNavigationButtons"]);
-    }, [apiResponse]);
+    const fetchSVGData = async (svgURL) => {
+      const data = await fetch(svgURL)
+        .then((response) => {
+          if (response.ok) return response.text();
+          return Promise.reject(response);
+        })
+        .then((data) => {
+          const regex = /fill="(.*?)"/g;
+          const modifiedData = data.replace(regex, `fill="#FFF"`);
+          return modifiedData;
+        });
+      return data;
+    };
 
-    if (listItems != null)
-        return (
-            listItems.map((item, index) => {
-                return (
-                    <a href={item.actionUrl} className="left-list-item" key={index}>
-                        <div className="icon">
-                            <img src={item.icon.url} className='leftPanelIcon'></img>
-                        </div>
-                        <div className="leftPanelItemTitle">{item.buttonText}</div>
-                    </a>
-                )
-            })
-        )
+    const fillSVGColor = async () => {
+      if (apiResponse?.["sideNavigationButtons"]) {
+        const promises = apiResponse?.["sideNavigationButtons"].map(
+          (navigationButton) => {
+            const svgURL = navigationButton.icon.url;
+            return fetchSVGData(svgURL);
+          }
+        );
+
+        const allSVGModifiedData = await Promise.all(promises);
+        console.log(allSVGModifiedData);
+        console.log(typeof allSVGModifiedData);
+        if (allSVGModifiedData) setListSVGs(allSVGModifiedData);
+        return allSVGModifiedData;
+      }
+    };
+
+    if (apiResponse?.["sideNavigationButtons"]) {
+      const modifiedSVGs = fillSVGColor();
+    }
+  }, [apiResponse]);
+
+  if (listItems != null && listSVGs != null)
+    return listItems.map((item, index) => {
+      return (
+        <a href={item.actionUrl} className="left-list-item" key={index}>
+          <div className="icon">
+            <img src={listSVGs[index]} className="leftPanelIcon"></img>
+          </div>
+          <div className="leftPanelItemTitle">{item.buttonText}</div>
+        </a>
+      );
+    });
 }
 export default ListItems;
